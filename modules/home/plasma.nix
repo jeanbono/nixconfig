894d3d1aaef254df53037917a9c51c31e0208498
jeanbono <pierre.fraisse@nebulous.fr>
2026-02-14 16:22:27 +0100

    Removed nordic theme and added topbar + dock

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -1,28 +1,39 @@
-{ pkgs, lib, config, ... }:
+{ lib, config, ... }:
 
 let
   cfg = config.modules.home.plasma;
 in
 {
-  options.modules.home.plasma.enable = lib.mkEnableOption "Thème Plasma Nordic + Papirus-Dark";
+  options.modules.home.plasma.enable = lib.mkEnableOption "Configuration Plasma (panels, layout)";
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      nordic
-      papirus-icon-theme
-    ];
-
     # Variables utiles Wayland / Electron (côté user)
     home.sessionVariables = {
       NIXOS_OZONE_WL = "1";
     };
 
-    programs.plasma.workspace = {
-      lookAndFeel = "Nordic";
-      colorScheme = "Nordic";
-      theme = "Nordic";
-      iconTheme = "Papirus-Dark";
-      cursor.theme = "Nordic-cursors";
-    };
+    programs.plasma.panels = [
+      # Top bar : menu | spacer | system tray | horloge
+      {
+        location = "top";
+        height = 32;
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.panelspacer"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+      # Dock en bas : icon-only task manager (apps)
+      {
+        location = "bottom";
+        height = 56;
+        floating = true;
+        lengthMode = "fit";
+        widgets = [
+          "org.kde.plasma.icontasks"
+        ];
+      }
+    ];
   };
 }
