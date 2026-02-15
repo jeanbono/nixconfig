23b94e820b919daef361595140d13fd080dd9c90
jeanbono <pierre.fraisse@nebulous.fr>
2026-02-15 10:34:53 +0100

    Fix start-hyprland

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -34,7 +34,7 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
           user = "greeter";
         };
       };
