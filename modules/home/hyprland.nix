21cfa8a9aae18db7a9ed6eea3d5a97ee23e93329
jeanbono <pierre.fraisse@nebulous.fr>
2026-02-16 09:41:12 +0100

    Fix config wlogout

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -475,36 +475,38 @@ in
     '';
 
     xdg.configFile."wlogout/layout".text = ''
-      {
-        "label": "lock",
-        "action": "hyprlock",
-        "text": "Lock",
-        "key": "l"
-      },
-      {
-        "label": "logout", 
-        "action": "loginctl terminate-session $XDG_SESSION_ID",
-        "text": "Logout",
-        "key": "e"
-      },
-      {
-        "label": "suspend",
-        "action": "systemctl suspend", 
-        "text": "Suspend",
-        "key": "s"
-      },
-      {
-        "label": "reboot",
-        "action": "systemctl reboot",
-        "text": "Reboot", 
-        "key": "r"
-      },
-      {
-        "label": "shutdown",
-        "action": "systemctl poweroff",
-        "text": "Shutdown",
-        "key": "p"
-      }
+      [
+        {
+          "label": "lock",
+          "action": "hyprlock",
+          "text": "Lock",
+          "key": "l"
+        },
+        {
+          "label": "logout", 
+          "action": "loginctl terminate-session $XDG_SESSION_ID",
+          "text": "Logout",
+          "key": "e"
+        },
+        {
+          "label": "suspend",
+          "action": "systemctl suspend", 
+          "text": "Suspend",
+          "key": "s"
+        },
+        {
+          "label": "reboot",
+          "action": "systemctl reboot",
+          "text": "Reboot", 
+          "key": "r"
+        },
+        {
+          "label": "shutdown",
+          "action": "systemctl poweroff",
+          "text": "Shutdown",
+          "key": "p"
+        }
+      ]
     '';
 
     # ── Hyprlock (écran de verrouillage) ────────────────────────
