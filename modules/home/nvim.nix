44485ac33250c9438fb8cede96bfd5804b663e15
Pierre Fraisse <pierre.fraisse@nebulous.fr>
2026-02-20 14:05:25 +0100

    Fix lua config for nvim

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -197,7 +197,6 @@ in
           },
           snippets = { preset = "luasnip" },
           completion = {
-            selection = { preselect = true },
             documentation = { auto_show = true, auto_show_delay_ms = 200 },
           },
         })
