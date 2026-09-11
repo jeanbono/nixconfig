ac42bf8f33f2801dfdd4b82c4712c3e666713b83
Pierre Fraisse <pierre.fraisse@nebulous.fr>
2026-09-11 22:38:11 +0200

    :bug: Cap keyboard volume increases at 100 percent

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -263,7 +263,7 @@ in
             ))
 
             -- Audio
-            hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+"), { locked = true, repeating = true })
+            hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume --limit 1.0 @DEFAULT_AUDIO_SINK@ 2%+"), { locked = true, repeating = true })
             hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"), { locked = true, repeating = true })
             hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),    { locked = true })
             hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"), { locked = true })
