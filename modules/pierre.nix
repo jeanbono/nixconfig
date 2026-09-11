f59cb88671c62dc936cf55bb39f436de28ca2088
Pierre Fraisse <pierre.fraisse@nebulous.fr>
2026-09-11 19:56:40 +0200

    :wrench: Remove Pierre from the input group

--- modules/pierre.nix
+++ modules/pierre.nix
@@ -35,7 +35,7 @@
 
     user = { ... }: {
       description = "pierre";
-      extraGroups = [ "video" "audio" "input" ];
+      extraGroups = [ "video" "audio" ];
       # Pinned rather than left to NixOS's implicit first-normal-user
       # allocation, so furnace.nix's /mnt/data mount can reference it
       # instead of hardcoding uid=1000.
